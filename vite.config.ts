5c57efcb263c4c67fc99a1eac689a6c71015a0cc
lexmin0412 <zhangle_dev@outlook.com>
2023-11-28 01:43:06 +0800

    chore(vite): add base

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { VarletUIResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/todo/',
   plugins: [
 		vue(),
 		Components({
